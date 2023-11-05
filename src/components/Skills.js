5c4b34682a0df92d033595ab80d6bc829c4d59e2
Anthony <asingiser@gmail.com>
2023-11-05 08:52:22 -0500

    add projects, links and tools

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,10 +13,10 @@ const Skills = (props) => {
             <h3 className="text-xs font-semibold uppercase mb-1">
               {item.name}
             </h3>
-            {item.keywords.map((keyword, kindex) => 
+            {item.keywords.map((keyword, index) => 
               <span
                 className="inline-flex mr-2 text-xs font-semibold text-opacity-70 text-black border px-1 rounded border-purple-100 bg-purple-50 dark:border-gray-900 dark:bg-gray-600"
-                key={kindex}
+                key={index}
               >
                 {keyword}
               </span>
